ca75b61599febd4fb26e51044d116152993cff3d
chaos <chaoschaos0909@googlemail.com>
2020-02-25 19:06:33 -0500

    hw changes for gen3

--- Src/setup.c
+++ Src/setup.c
@@ -571,7 +571,7 @@ void MX_ADC1_Init(void) {
   sConfig.Rank    = 3;
   HAL_ADC_ConfigChannel(&hadc1, &sConfig);
 
-  sConfig.Channel = ADC_CHANNEL_12;  // pc2 vbat
+  sConfig.Channel = ADC_CHANNEL_1;  // pc2 vbat
   sConfig.Rank    = 4;
   HAL_ADC_ConfigChannel(&hadc1, &sConfig);
 
